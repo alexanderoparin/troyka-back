b4499ddf874040333109e8b6ed08c316fe7a2083
Alexander Oparin <alexanderoparin@mail.ru>
2025-09-13 19:05:21 +0300

    Initial commit

---
+++ src/main/java/ru/oparin/solving/SolvingApplication.java
@@ -0,0 +1,13 @@
+package ru.oparin.solving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvingApplication.class, args);
+    }
+
+}
