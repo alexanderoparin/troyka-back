91c407ba956e321d13a862050d9712f4a8321825
Alexander Oparin <alexanderoparin@mail.ru>
2025-10-10 18:19:33 +0300

    фикс разрыва соединения с бд

---
+++ src/main/java/ru/oparin/troyka/config/DatabaseConfig.java
@@ -0,0 +1,41 @@
+package ru.oparin.troyka.config;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    @Bean
+    public DatabaseClient databaseClient() {
+        return DatabaseClient.create(connectionFactory);
+    }
+
+    @Bean
+    public R2dbcEntityTemplate r2dbcEntityTemplate() {
+        return new R2dbcEntityTemplate(connectionFactory);
+    }
+
+    /**
+     * Обертка для Mono с retry логикой при потере связи с БД
+     */
+    public static <T> Mono<T> withRetry(Mono<T> mono) {
+        return mono.retryWhen(Retry.backoff(3, Duration.ofSeconds(1))
+                .maxBackoff(Duration.ofSeconds(5))
+                .jitter(0.1)
+                .doBeforeRetry(retrySignal -> 
+                    System.out.println("Retrying database operation, attempt: " + retrySignal.totalRetries())
+                ));
+    }
+}
