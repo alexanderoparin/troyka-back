c590737af4555a274079bfa9b327278b93ab0bfa
Alexander Oparin <alexanderoparin@mail.ru>
2025-10-11 21:01:50 +0300

    сессия -> секьюрити

--- src/main/java/ru/oparin/troyka/security/SecurityConfig.java
+++ src/main/java/ru/oparin/troyka/security/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
                         .pathMatchers("/files/**").permitAll()
                         .pathMatchers("/fal/**").authenticated()
                         .pathMatchers("/users/**").authenticated()
+                        .pathMatchers("/api/sessions/**").authenticated()
                         .pathMatchers("/payment/**").permitAll()
                         .anyExchange().authenticated()
                 )
