2603b83c1925ae90345fb5849834c4fe2d341e5a
alex <alexandroparin09@gmail.com>
2025-11-28 20:58:20 +0300

    включил аудит

--- src/main/java/ru/oparin/troyka/config/DatabaseConfig.java
+++ src/main/java/ru/oparin/troyka/config/DatabaseConfig.java
@@ -4,6 +4,7 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.r2dbc.core.DatabaseClient;
 import reactor.core.publisher.Mono;
@@ -12,6 +13,7 @@ import reactor.util.retry.Retry;
 import java.time.Duration;
 
 @Configuration
+@EnableR2dbcAuditing
 public class DatabaseConfig {
 
     @Autowired
